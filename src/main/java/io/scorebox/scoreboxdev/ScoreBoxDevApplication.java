c91e730ff35cafab7a9e7c896684e3876caef4d8
dzianis.monich <dzianis.monich@leverx.com>
2019-10-18 15:42:02 +0300

    added postgres settings

--- src/main/java/io/scorebox/scoreboxdev/ScoreBoxDevApplication.java
+++ src/main/java/io/scorebox/scoreboxdev/ScoreBoxDevApplication.java
@@ -2,9 +2,11 @@ package io.scorebox.scoreboxdev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // CHECKSTYLE:OFF
 @SpringBootApplication
+@EnableJpaAuditing
 public class ScoreBoxDevApplication {
 
     public static void main(String[] args) {
